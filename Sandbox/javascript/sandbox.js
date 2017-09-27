ad35ef4f0776610e33b7ddb670d896ab41a44035
Guzman Monne <guzmonne@hotmail.com>
2017-09-27 15:52:30 -0300

    prueba

--- Sandbox/javascript/sandbox.js
+++ Sandbox/javascript/sandbox.js
@@ -1,7 +1,64 @@
-(function(){
-  function log() { console.log('iife1'); }
-})()
+var foo = "bar";
 
-(function(w){
-  function log() { console.log('iife2'); }
-})(window || global)
+console.comment('bar');
+function bar() {
+  // Shadowing.
+  console.log(foo);
+  // >>> ¿?
+  var foo = "baz";
+  console.log(foo);
+  // >>> "baz"
+  return 123;
+}
+
+console.comment('bar()');
+console.log(bar());
+// >>> ¿?
+console.comment('foo');
+console.log(foo);
+
+function baz(foo) {
+  foo = "bam";
+  bam = "yay";
+  return {};
+}
+
+console.comment('IIFE');
+(function IIFE(a, b, c, bar) {
+  var foo = 'foo2'
+  console.log(a);
+  // >>> 1
+  console.log(b);
+  // >>> 2
+  console.log(c);
+  // >>> 3
+  console.log(bar);
+  // >>> bar
+})(1, 2, 3, foo);
+
+
+var foos = function bat() {
+  function nas() {
+    var foos = foo = bat
+  }
+  console.comment('bat.toString()');
+  console.log(bat.toString());
+  nas();
+  // >>> ¿?
+};
+
+foos();
+
+console.comment('foo.toString()');
+console.log(foo.toString());
+
+console.comment('bar.toString()');
+console.log(bar.toString());
+
+function bin() {
+  return bos();
+  // ---
+  function bos() {
+    // ...
+  }
+}
